9a9aa0ac49513e30c6b464c64b1f4149436ea9c8
Max Kremmel <xing@synapse.plus.com>
2006-05-06 15:48:05 +0000

    clean up schema files, remove unused PackageInfo data - we really need a package manager for this, add plugin_path column to liberty_plugins

---
+++ admin/schema_inc.php
@@ -0,0 +1,6 @@
+<?php
+$gBitInstaller->registerPackageInfo( CATEGORIES_PKG_NAME, array(
+	'description' => "AJAX based spell checker that is available when you edit text.",
+	'license' => '<a href="http://www.gnu.org/licenses/licenses.html#LGPL">LGPL</a>',
+) );
+?>
