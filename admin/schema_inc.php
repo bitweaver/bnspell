1fe261a711f1f818f1d358cc02ce217d68b9e77d
Christian Fowler <spider@viovio.com>
2006-06-05 20:01:49 +0000

    fix package name

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -1,5 +1,5 @@
 <?php
-$gBitInstaller->registerPackageInfo( CATEGORIES_PKG_NAME, array(
+$gBitInstaller->registerPackageInfo( BNSPELL_PKG_NAME, array(
 	'description' => "AJAX based spell checker that is available when you edit text.",
 	'license' => '<a href="http://www.gnu.org/licenses/licenses.html#LGPL">LGPL</a>',
 ) );
