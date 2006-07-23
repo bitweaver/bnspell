81962374fb3c7144af244feea3a611da2102b5ce
Max Kremmel <xing@synapse.plus.com>
2006-07-23 08:04:50 +0000

    update globals in schema files

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -1,4 +1,5 @@
 <?php
+global $gBitInstaller;
 $gBitInstaller->registerPackageInfo( BNSPELL_PKG_NAME, array(
 	'description' => "AJAX based spell checker that is available when you edit text.",
 	'license' => '<a href="http://www.gnu.org/licenses/licenses.html#LGPL">LGPL</a>',
