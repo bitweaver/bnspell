cabafe957473d9bd9cafccd912a1df1b7b75878f
Christian Fowler <spider@viovio.com>
2008-06-25 22:21:07 +0000

    change display call to use a hash for display options, add display_mode to options hash

--- example.php
+++ example.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_bnspell/example.php,v 1.4 2008/06/19 06:11:10 lsces Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_bnspell/example.php,v 1.5 2008/06/25 22:21:07 spiderr Exp $
  *
  * Copyright (c) 2005, Garrison Locke
  * @author Garrison Locke
@@ -14,6 +14,6 @@
  */
 require_once( '../bit_setup_inc.php' );
 
-$gBitSystem->display( 'bitpackage:bnspell/bnspell_example.tpl' );
+$gBitSystem->display( 'bitpackage:bnspell/bnspell_example.tpl' , NULL, array( 'display_mode' => 'display' ));
 
 ?>
