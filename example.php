568d0be57ad0217dce86cf92fa2502989c714387
wjames5 <will@tekimaki.com>
2010-02-08 21:27:21 +0000

    deprecate root bit_setup_inc use kernel/setup_inc.php

--- example.php
+++ example.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_bnspell/example.php,v 1.5 2008/06/25 22:21:07 spiderr Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_bnspell/example.php,v 1.6 2010/02/08 21:27:21 wjames5 Exp $
  *
  * Copyright (c) 2005, Garrison Locke
  * @author Garrison Locke
@@ -12,7 +12,7 @@
 /**
  * Initialize
  */
-require_once( '../bit_setup_inc.php' );
+require_once( '../kernel/setup_inc.php' );
 
 $gBitSystem->display( 'bitpackage:bnspell/bnspell_example.tpl' , NULL, array( 'display_mode' => 'display' ));
 
