6d297d2dc40aa97d58244c2bc430eb846baf5b29
spiderr <spider@viovio.com>
2013-03-29 17:15:44 -0400

    send missing pspell message to error_log

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -7,7 +7,7 @@ $gBitSystem->registerPackage( $registerHash );
 
 if( $gBitSystem->isPackageActive( 'bnspell' ) ) {
 	if( !function_exists( 'pspell_config_create' ) ) {
-		vd( 'bnspell cannot find pspell functions' );
+		error_log( 'bnspell cannot find pspell functions' );
 	}
 //	$gBitSystem->registerAppMenu( BNSPELL_PKG_NAME, "Spell Checking", BNSPELL_PKG_URL.'index.php', 'bitpackage:bnspell/menu_bnspell.tpl', BNSPELL_PKG_NAME );
 }
